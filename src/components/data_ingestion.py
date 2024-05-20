d4be1a7686e7cc3cce53a9ec3eedf0d9be556f06
Neel Patel <np.patelneel@gmail.com>
2024-05-19 18:06:56 -0700

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,49 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts', 'train.csv')
+    test_data_path: str=os.path.join('artifacts', 'test.csv')
+    raw_data_path: str=os.path.join('artifacts', 'raw_data.csv')
+    
+    
+class DataIngestion:
+    def __init__ (self):
+        self.ingesion_config = DataIngestionConfig()
+        
+    def initiate_data_ingestion(self):
+        logging.info("Data ingestion initiated")
+        try:
+            df=pd.read_csv('notebook/data/stud.csv')
+            logging.info("Read the dataset as dataframe")
+            
+            train, test = train_test_split(df, test_size=0.2, random_state=42)
+            logging.info("Split the dataset into train and test")
+            
+            train.to_csv(self.ingesion_config.train_data_path, index=False, header=True)
+            test.to_csv(self.ingesion_config.test_data_path, index=False, header=True)
+            df.to_csv(self.ingesion_config.raw_data_path, index=False, header=True)
+            
+            logging.info("Data ingestion completed")
+            
+            return (
+                self.ingesion_config.train_data_path,
+                self.ingesion_config.test_data_path,
+                self.ingesion_config.raw_data_path
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
+          
+
+if __name__ == '__main__':
+    data_ingestion = DataIngestion()
+    data_ingestion.initiate_data_ingestion()
+
